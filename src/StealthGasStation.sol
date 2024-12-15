5df18cd167f7780c80e37e80bddc6cbcb1f863ce
κασσάνδρα.eth <0xDADA@protonmail.com>
2024-12-15 14:43:33 +0100

    chore: clean up contract and tests

--- src/StealthGasStation.sol
+++ src/StealthGasStation.sol
@@ -5,9 +5,11 @@ contract StealthGasStation {
     error WrongValue();
     error OnlyCoordinator();
     error TransferFailed();
+    error LengthMismatch();
 
     event BuyGasTickets(bytes[] blinded);
-    event RemitGasTickets(bytes[] signed);
+    event RemitGasTickets(bytes32[] hashes, bytes[] signed);
+    event NativeTransfers(uint256[] amounts, address[] targets);
 
     address public immutable coordinator;
     uint256 public immutable ticketSize;
@@ -25,16 +27,26 @@ contract StealthGasStation {
         emit BuyGasTickets(blindedData);
     }
 
-    function remitGasTickets(bytes[] calldata blindSigned) external {
+    function remitGasTickets(bytes32[] calldata hashes, bytes[] calldata blindSigned) external {
         if (msg.sender != coordinator) revert OnlyCoordinator();
 
-        emit RemitGasTickets(blindSigned);
+        uint256 len = hashes.length;
+        if (len != blindSigned.length) revert LengthMismatch();
+
+        emit RemitGasTickets(hashes, blindSigned);
     }
 
-    function sendGas(uint256 amount, address to) external {
+    function sendGas(uint256[] calldata amounts, address[] calldata targets) external {
         if (msg.sender != coordinator) revert OnlyCoordinator();
+        
+        uint256 len = amounts.length;
+        if (len != targets.length) revert LengthMismatch();
+        
+        for (uint256 i = 0; i < len; i++) {
+            (bool success, ) = payable(targets[i]).call{value: amounts[i]}("");
+            if (!success) revert TransferFailed();
+        }
 
-        (bool success, ) = payable(to).call{value: amount}("");
-        if (!success) revert TransferFailed();
+        emit NativeTransfers(amounts, targets);
     }
 }
